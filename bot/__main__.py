dbc62af2c82e393620499dfb46b33814976b2eb0
Shivaprasad <91235488+Ssfive123@users.noreply.github.com>
2022-08-06 20:25:04 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -41,22 +41,22 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>Version:</b> 2.4.2022\n\n'\
-            f'<b>Bot Uptime:</b> {currentTime}\n'\
-            f'<b>OS Uptime:</b> {osUptime}\n\n'\
-            f'<b>Total Disk Space:</b> {total}\n'\
-            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
-            f'<b>Upload:</b> {sent}\n'\
-            f'<b>Download:</b> {recv}\n\n'\
-            f'<b>CPU:</b> {cpuUsage}%\n'\
-            f'<b>RAM:</b> {mem_p}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
-            f'<b>Physical Cores:</b> {p_core}\n'\
-            f'<b>Total Cores:</b> {t_core}\n\n'\
-            f'<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
-            f'<b>Memory Total:</b> {mem_t}\n'\
-            f'<b>Memory Free:</b> {mem_a}\n'\
-            f'<b>Memory Used:</b> {mem_u}\n'
+    stats = f'<b>📟 Version:</b> 2.4.2022\n\n'\
+            f'<b>🤖 Bot Uptime:</b> {currentTime}\n'\
+            f'<b>🖲️ OS Uptime:</b> {osUptime}\n\n'\
+            f'<b>🖥️ Total Disk Space:</b> {total}\n'\
+            f'<b>🔴 Used:</b> {used} | <b>Free:</b> {free}\n\n'\
+            f'<b>📤 Upload:</b> {sent}\n'\
+            f'<b>📥 Download:</b> {recv}\n\n'\
+            f'<b>🎛️ CPU:</b> {cpuUsage}%\n'\
+            f'<b>🔍 RAM:</b> {mem_p}%\n'\
+            f'<b>🔎 DISK:</b> {disk}%\n\n'\
+            f'<b>🕯️ Physical Cores:</b> {p_core}\n'\
+            f'<b>🏮 Total Cores:</b> {t_core}\n\n'\
+            f'<b>🗳️ SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
+            f'<b>🗂️ Memory Total:</b> {mem_t}\n'\
+            f'<b>📂 Memory Free:</b> {mem_a}\n'\
+            f'<b>📁 Memory Used:</b> {mem_u}\n'
     sendMessage(stats, context.bot, update.message)
 
 
